412c8791bd66fd5921b839577cd8298f44b87ac0
kiruse <26940742+Kiruse@users.noreply.github.com>
2024-06-04 09:37:35 +0200

    upgrade to new marshal

--- src/marshal.ts
+++ src/marshal.ts
@@ -3,13 +3,13 @@ import { ObjectId } from 'mongodb';
 import { omit } from './util.js';
 
 export const MongoIDRenamer = defineMarshalUnit(
-  (value: any, passback) => typeof value === 'object' && value?.id instanceof ObjectId
+  (value: any, { marshal }) => typeof value === 'object' && value?.id instanceof ObjectId
     //@ts-ignore
-    ? morph(passback({ _id: value.id, ...omit(value, 'id') }))
+    ? morph(marshal({ _id: value.id, ...omit(value, 'id') }))
     : pass,
-  (value: any, passback) => typeof value === 'object' && value?._id instanceof ObjectId
+  (value: any, { unmarshal }) => typeof value === 'object' && value?._id instanceof ObjectId
     //@ts-ignore
-    ? morph(passback({ id: value._id, ...omit(value, '_id') }))
+    ? morph(unmarshal({ id: value._id, ...omit(value, '_id') }))
     : pass,
   true,
 );
